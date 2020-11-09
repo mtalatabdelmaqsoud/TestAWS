b98ee98adc82eb74083bd08ede40650dbb4ebc2d
mtabdelmaqsoud <mtabdelmaqsoud@gmail.com>
2020-11-09 18:12:58 +0100

    coap demo and .cproject update (new)

--- vendors/st/boards/stm32l475_discovery/aws_demos/config_files/aws_demo_config.h
+++ vendors/st/boards/stm32l475_discovery/aws_demos/config_files/aws_demo_config.h
@@ -30,52 +30,45 @@
  * Only one demo can be configured at a time
  *
  *          CONFIG_MQTT_DEMO_ENABLED
+ *          CONFIG_MQTT_TRANSPORT_DEMO_ENABLED
  *          CONFIG_CORE_MQTT_BASIC_TLS_DEMO_ENABLED
  *          CONFIG_CORE_MQTT_MUTUAL_AUTH_DEMO_ENABLED
  *          CONFIG_CORE_MQTT_KEEP_ALIVE_DEMO_ENABLED
  *          CONFIG_CORE_MQTT_PLAINTEXT_DEMO_ENABLED
  *          CONFIG_CORE_MQTT_SERIALIZER_DEMO_ENABLED
  *          CONFIG_CORE_MQTT_CONNECTION_SHARING_DEMO_ENABLED
- *          CONFIG_DEVICE_SHADOW_DEMO_ENABLED
  *          CONFIG_SHADOW_DEMO_ENABLED
+ *          CONFIG_DEVICE_SHADOW_DEMO_ENABLED
  *          CONFIG_GREENGRASS_DISCOVERY_DEMO_ENABLED
  *          CONFIG_TCP_ECHO_CLIENT_DEMO_ENABLED
  *          CONFIG_DEFENDER_DEMO_ENABLED
- *          CONFIG_POSIX_DEMO_ENABLED
  *          CONFIG_OTA_UPDATE_DEMO_ENABLED
+ *          CONFIG_BLE_GATT_SERVER_DEMO_ENABLED
+ *          CONFIG_MQTT_BLE_TRANSPORT_DEMO_ENABLED
+ *          CONFIG_SHADOW_BLE_TRANSPORT_DEMO_ENABLED
  *          CONFIG_HTTPS_SYNC_DOWNLOAD_DEMO_ENABLED
  *          CONFIG_HTTPS_ASYNC_DOWNLOAD_DEMO_ENABLED
  *          CONFIG_HTTPS_SYNC_UPLOAD_DEMO_ENABLED
  *          CONFIG_HTTPS_ASYNC_UPLOAD_DEMO_ENABLED
  *          CONFIG_CLI_UART_DEMO_ENABLED
- *
+ *          CONFIG_COAP_DEMO_ENABLED
+
  *  These defines are used in iot_demo_runner.h for demo selection */
 
-#define CONFIG_MQTT_DEMO_ENABLED
+/*Select CoAP demo*/
+#define CONFIG_COAP_DEMO_ENABLED
 
 /* Default configuration for all demos. Individual demos can override these below */
-#define democonfigDEMO_STACKSIZE                                     ( configMINIMAL_STACK_SIZE * 8 )
-#define democonfigDEMO_PRIORITY                                      ( tskIDLE_PRIORITY + 5 )
-#define democonfigNETWORK_TYPES                                      ( AWSIOT_NETWORK_TYPE_WIFI )
+#define democonfigDEMO_STACKSIZE    ( configMINIMAL_STACK_SIZE * 8 )
+#define democonfigDEMO_PRIORITY     ( tskIDLE_PRIORITY + 5 )
+#define democonfigNETWORK_TYPES     ( AWSIOT_NETWORK_TYPE_WIFI )
 
-#define democonfigSHADOW_DEMO_NUM_TASKS                              ( 2 )
-#define democonfigSHADOW_DEMO_TASK_STACK_SIZE                        ( 1024 )
-#define democonfigSHADOW_DEMO_TASK_PRIORITY                          ( tskIDLE_PRIORITY + 5 )
-#define shadowDemoUPDATE_TASK_STACK_SIZE                             ( configMINIMAL_STACK_SIZE * 5 )
+#if defined( CONFIG_MQTT_DEMO_ENABLED )
+    #undef democonfigNETWORK_TYPES
+    #define democonfigNETWORK_TYPES    ( AWSIOT_NETWORK_TYPE_WIFI | AWSIOT_NETWORK_TYPE_BLE )
+#endif
 
-#define democonfigMQTT_ECHO_TLS_NEGOTIATION_TIMEOUT                  pdMS_TO_TICKS( 12000 )
-#define democonfigMQTT_ECHO_TASK_PRIORITY                            ( tskIDLE_PRIORITY )
-
-/* Timeout used when performing MQTT operations that do not need extra time
- * to perform a TLS negotiation. */
-#define democonfigMQTT_TIMEOUT                                       pdMS_TO_TICKS( 2500 )
-
-/* Send AWS IoT MQTT traffic encrypted to destination port 443. */
-#define democonfigMQTT_AGENT_CONNECT_FLAGS                           ( mqttagentREQUIRE_TLS )
-
-/* Greengrass discovery example task parameters. */
-#define democonfigGREENGRASS_DISCOVERY_TASK_STACK_SIZE               ( configMINIMAL_STACK_SIZE * 22 )
-#define democonfigGREENGRASS_DISCOVERY_TASK_PRIORITY                 ( tskIDLE_PRIORITY )
+#define democonfigGREENGRASS_DISCOVERY_TASK_STACK_SIZE               ( configMINIMAL_STACK_SIZE * 12 )
 
 /* MQTT Connection sharing demo task priority. */
 #define democonfigCORE_MQTT_CONNECTION_SHARING_DEMO_TASK_PRIORITY    ( tskIDLE_PRIORITY + 1 )
@@ -86,11 +79,12 @@
     #define democonfigMEMORY_ANALYSIS_STACK_DEPTH_TYPE    UBaseType_t
     #define democonfigMEMORY_ANALYSIS_MIN_EVER_HEAP_SIZE()        xPortGetMinimumEverFreeHeapSize()
     #if ( INCLUDE_uxTaskGetStackHighWaterMark == 1 )
-        /* Convert from stack words to bytes */
-        #define democonfigMEMORY_ANALYSIS_STACK_WATERMARK( x )    uxTaskGetStackHighWaterMark( x ) * ( uint32_t ) sizeof( StackType_t ); /*lint !e961 Casting is not redundant on smaller architectures. */
+        /* Shift by left twice to convert from stack words to bytes */
+        #define democonfigMEMORY_ANALYSIS_STACK_WATERMARK( x )    uxTaskGetStackHighWaterMark( x )
     #else
         #define democonfigMEMORY_ANALYSIS_STACK_WATERMARK( x )    NULL
     #endif /* if( INCLUDE_uxTaskGetStackHighWaterMark == 1 ) */
 #endif /* democonfigMEMORY_ANALYSIS */
+
 
 #endif /* _AWS_DEMO_CONFIG_H_ */
